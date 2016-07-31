55127b609368209288ba69a80c37f02e685694ef
Patrick Poirson <poirson@cs.unc.edu>
2016-07-31 18:11:34 -0400

    official eval

--- avp_eval.m
+++ avp_eval.m
@@ -13,19 +13,41 @@ classes = {'bicycle',
 
           
 aps = [];
+aas = [];
 
 bins = 8;
 
+
 for i=1:length(classes)
     cls = classes{i};
-    fname = [cls '.mat'];
+    fname = fullfile(path, [cls '.mat']);
     
     [recall, precision, accuracy, ap, aa] = compute_recall_precision_accuracy_azimuth(cls, bins, bins, fname);
     %ap = 0;
-    disp(ap)
-    aps = [aps ap];
+    %disp(ap)
+    %aps = [aps ap];
+    aas = [aas aa];
 end
 
-disp(aps)
-mean(aps)
+
+fid = fopen(fullfile(path, 'results.txt'), 'w');
+for i=1:length(classes)
+    cls = classes{i};
+    fprintf(fid, sprintf('%s', cls));
+    spaces = 15 - length(cls);
+    for j=1:spaces
+        fprintf(fid, ' ');
+    end
+end
+fprintf(fid, '\n');
+
+%disp(aas(2))
+for i=1:length(classes)
+    fprintf(fid, sprintf('%.1f           ', aas(i)*100));
+end
+fprintf(fid, '\n');
+fprintf(fid, sprintf('Mean = %.1f\n', mean(aas)*100));
+
+fclose(fid);
+
 
---
+++ runOfficialTest.py
@@ -0,0 +1,148 @@
+import os
+import sys
+sys.path.insert(0, 'python')
+import caffe
+caffe.set_mode_gpu()
+caffe.set_device(0)
+from google.protobuf import text_format
+from caffe.proto import caffe_pb2
+import json
+import os.path as osp
+import scipy.io as sio
+import argparse
+import numpy as np
+
+
+
+def main(args):
+  
+  voc_labelmap_file = 'data/pascal3D/labelmap_3D.prototxt'
+  file = open(voc_labelmap_file, 'r')
+  voc_labelmap = caffe_pb2.LabelMap()
+  text_format.Merge(str(file.read()), voc_labelmap)
+
+
+  mod_fi = args['model']
+  iterx = args['iter']
+  modelW = 'VGG_Pascal3D_%s_iter_%d.caffemodel' % (mod_fi, iterx)
+  model_def = osp.join('models/VGGNet/Pascal3D/', mod_fi, 'deploy.prototxt')
+  model_weights = osp.join('models/VGGNet/Pascal3D/', mod_fi, modelW)
+
+  net = caffe.Net(model_def, model_weights, caffe.TEST)     
+  transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+  transformer.set_transpose('data', (2, 0, 1))
+  transformer.set_mean('data', np.array([104,117,123])) # mean pixel
+  transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+  transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+
+  f = open('data/pascal3D/PASCAL/VOCdevkit/VOC2012/ImageSets/Main/val.txt', 'r')
+  val = [line.strip('\n') for line in f ]
+
+  all_out = {}
+
+  # hack 
+  fiOutput = '%s_%d' % (mod_fi, iterx)
+  skip = False
+  if osp.exists(osp.join('mat_eval', fiOutput, 'car.mat')):
+    skip = True
+
+  for idx, v in enumerate(val):
+      if skip:
+        break
+
+      imfile = osp.join('data/pascal3D/images/pascal/', v + '.jpg')
+
+      # set net to batch size of 1
+      image_resize = 300
+      net.blobs['data'].reshape(1,3,image_resize,image_resize)
+      
+      # hack ??
+      if not osp.exists(imfile):
+          imfile = osp.join('data/pascal3D/PASCAL/VOCdevkit/VOC2012/JPEGImages/', v + '.jpg')
+          #print 'why'
+      
+      image = caffe.io.load_image(imfile)
+
+      #plt.imshow(image)
+
+
+      transformed_image = transformer.preprocess('data', image)
+      net.blobs['data'].data[...] = transformed_image
+
+      # Forward pass.
+      detections = net.forward()['detection_out']
+
+      # Parse the outputs.
+      det_label = get_labelname(voc_labelmap, detections[0,0,:,1].tolist())
+      out = np.zeros((detections.shape[2], 6))
+      out[:, 0] = detections[0,0,:,3]*image.shape[1]
+      out[:, 1] = detections[0,0,:,4]*image.shape[0]
+      out[:, 2] = detections[0,0,:,5]*image.shape[1]
+      out[:, 3] = detections[0,0,:,6]*image.shape[0]
+      out[:, 4] = detections[0,0,:,7]
+      out[:, 5] = detections[0,0,:,2]
+          
+      cur_out = {'bicycle':[],
+                 'motorbike': [],
+                 'car':[],
+                 'bus':[],
+                 'train':[],
+                'diningtable':[],
+                'aeroplane':[],
+                'sofa':[],
+                'tvmonitor':[],
+                'chair':[],
+                'boat':[]}
+      
+      for det_idx, lbl in enumerate(det_label):
+          cur_out[lbl].append(out[det_idx,:])
+      
+      for lbl, v in cur_out.iteritems():
+          if lbl not in all_out:
+              all_out[lbl] = []
+          all_out[lbl].append(v)
+      
+      if idx % 100 == 0:
+          print '%d/%d' % (idx, len(val)) 
+
+  for lbl, all_val in all_out.iteritems():
+      if skip:
+        break
+      if not osp.exists('mat_eval'):
+        os.mkdir('mat_eval')
+        os.mkdir(osp.join('mat_eval', fiOutput))
+      if not osp.exists(osp.join('mat_eval', fiOutput)):
+        os.mkdir(osp.join('mat_eval', fiOutput))
+
+      out_file = osp.join('mat_eval', fiOutput, lbl + '.mat')
+      sio.savemat(out_file, {'dets': all_val})
+
+  matlab_cmd = 'bins = %d; path = \'%s\'; avp_eval;' % (args['bins'], osp.join('mat_eval', fiOutput))
+  print matlab_cmd
+  os.system('matlab -nodisplay -r "try %s catch; end; quit;"' % (matlab_cmd))
+
+
+def get_labelname(labelmap, labels):
+    num_labels = len(labelmap.item)
+    labelnames = []
+    if type(labels) is not list:
+        labels = [labels]
+    for label in labels:
+        found = False
+        for i in xrange(0, num_labels):
+            if label == labelmap.item[i].label:
+                found = True
+                labelnames.append(labelmap.item[i].display_name)
+                break
+        assert found == True
+    return labelnames
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test an SSD model ")
+    parser.add_argument('--model', type=str, help='model name')
+    parser.add_argument('--iter', type=int, help='iteration to test')
+    parser.add_argument('--bins', default=8, type=int, help='number of bins')
+    
+    args = parser.parse_args()
+    params = vars(args)
+    main(params)
