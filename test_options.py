bafe42a96d72e007ab8cbff66497c95661f2329f
Patrick Poirson <poirson@cs.unc.edu>
2016-08-23 17:29:29 -0400

    update readme. cleanup

--- test_options.py
+++
@@ -1,7 +0,0 @@
-#from ssd_pose.utils import options
-from utils import options
-
-if __name__ == "__main__":
-	test = options.Options('configs/test.json')
-	print test.get_db_name_stem('train')
-	test.dummy_test()
