b02fb2f4dd093c1b2ced3fe09068d50d93649892
Patrick Poirson <poirson@cs.unc.edu>
2016-10-13 22:03:54 -0400

    bug

--- obj_job.py
+++ obj_job.py
@@ -15,8 +15,28 @@ def main(args):
 
     if args['idx'] != 0 and opt_dir == '':
         opt_dir = osp.join('/home/poirson/options/', str(args['idx']) + '.json')
-    
+
     opt = options.Options(opt_dir)
+
+    data_root_dir='data/ObjectNet3D'
+
+    trstem = opt.get_objnet_db_stem('train') 
+    trdb = ('%s_lmdb' % trstem, trstem, 'train.txt')
+
+    valstem = opt.get_objnet_db_stem('val') 
+    valdb = ('%s_lmdb' % valstem, valstem, 'val.txt')
+
+    testem = opt.get_objnet_db_stem('test') 
+    testdb = ('%s_lmdb' % testem, testem, 'test.txt')
+
+    with open(osp.join(data_root_dir, 'cache', valstem, 'val.txt'), 'r') as infile:
+        numVal = len([line for line in infile])
+        opt.add_kv('num_val', numVal)
+
+    with open(osp.join(data_root_dir, 'cache', testem, 'test.txt'), 'r') as infile:
+        numTest = len([line for line in infile]) 
+        opt.add_kv('num_test', numTest)
+
 
     mod_id = random.randint(1, 999000)
     if args['idx'] != 0:
